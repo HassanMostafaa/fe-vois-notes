b36723c58d76a76dc9ca10200f740ceca7161227
Hassan <hassan.bmw2251@gmail.com>
2024-07-18 04:25:43 +0300

    pre style load fix

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,15 +1,15 @@
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import { FunctionComponent, PropsWithChildren } from "react";
-// import { SLayout } from "./styles/Layout.styles";
+import SPageLayout from "./styles/PageLayout.styles";
 
 const PageLayout: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <div>
+    <SPageLayout style={{ opacity: 0 }}>
       <Navbar />
       <main>{children}</main>
       <Footer />
-    </div>
+    </SPageLayout>
   );
 };
 
---
+++ src/components/layout/styles/PageLayout.styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const SPageLayout = styled.div`
+  opacity: 1 !important;
+`;
+export default SPageLayout;
