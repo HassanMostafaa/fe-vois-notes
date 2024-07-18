d8198daaf0a31c00010b14fe256099bf91670ee4
Hassan <hassan.bmw2251@gmail.com>
2024-07-18 04:42:17 +0300

    ios support: normal bg

--- src/components/footer/styles/Footer.styles.ts
+++ src/components/footer/styles/Footer.styles.ts
@@ -3,8 +3,12 @@ import { styled } from "styled-components";
 
 export const SFooter = styled.footer`
   position: fixed;
-  padding: 12px;
+  padding: 8px;
   bottom: 0;
   right: 0;
-  backdrop-filter: blur(8px);
+  background-color: #f4f4f4;
+  width: 100%;
+  text-align: right;
+  box-shadow: 0 0 10px #00000022;
+  font-size: 14px;
 `;
